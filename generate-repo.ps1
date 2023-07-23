c53080d4ee6a6c1f7d1e9b40dfcf9531c5446fa8
Asriel Camora <asriel@camora.dev>
2023-07-23 10:16:03 +0400

    I don't know how to use powershell

--- generate-repo.ps1
+++ generate-repo.ps1
@@ -1,3 +1,16 @@
+function GetAssetByType($assets, $type) {
+  foreach ($asset in $assets) {
+    if ($type -eq $asset.content_type) {
+      $asset
+    }
+  }
+}
+
+function ExitWithCode($code) {
+  $host.SetShouldExit($code)
+  exit $code
+}
+
 $pluginsOut = @()
 
 $pluginList = Get-Content '.\repos.json' | ConvertFrom-Json
@@ -12,34 +25,33 @@ foreach ($plugin in $pluginList) {
   $json = ConvertFrom-Json $data.content
 
   # Get data from the api request.
-  $downloadAsset = GetAssetByName($json.assets, "application/zip")
-  $configAsset = GetAssetByName($json.assets, "application/json")
+  $downloadAsset = GetAssetByType $json.assets "application/zip"
+  $configAsset = GetAssetByType $json.assets "application/json"
 
   if ($null -eq $downloadAsset) {
     Write-Error "Download asset for plugin $($plugin) is null!"
-    ExitWithCode(1)
+    ExitWithCode 1
   }
 
   if ($null -eq $configAsset) {
     Write-Error "Config asset for plugin $($plugin) is null!"
-    ExitWithCode(1)
+    ExitWithCode 1
   }
 
-  $downloadCount = $jdownloadAsset.download_count
+  $downloadCount = $downloadAsset.download_count
   $downloadUrl = $downloadAsset.browser_download_url
 
   # Get timestamp for the release.
   $releaseTimestamp = [Int](New-TimeSpan -Start (Get-Date "01/01/1970") -End ([DateTime]$json.published_at)).TotalSeconds
 
   # Get the config data from the release data.
-  $configUrl = $configAsset.browser_download_url
-  $configData = Invoke-WebRequest -Uri $manifestUrl
-  $config = ConvertFrom-Json $manifestData.content
+  $configData = Invoke-WebRequest -Uri $configAsset.browser_download_url
+  $config = ConvertFrom-Json ([System.Text.Encoding]::UTF8.GetString($configData.content))
 
   # Ensure that config is converted properly.
   if ($null -eq $config) {
     Write-Error "Config for plugin $($plugin) is null!"
-    ExitWithCode(1)
+    ExitWithCode 1
   }
 
   # Add additional properties to the config.
@@ -60,17 +72,3 @@ $pluginJson = ConvertTo-Json $pluginsOut
 
 # Save repo to file
 Set-Content -Path "plogon.json" -Value $pluginJson
-
-function GetAssetByType($assets, $type) {
-  foreach ($asset in $assets) {
-    if ($type -eq $asset.content_type) {
-      return $asset
-    }
-  }
-}
-
-# Function to exit with a specific code.
-function ExitWithCode($code) {
-  $host.SetShouldExit($code)
-  exit $code
-}
